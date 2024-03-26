b4941bfe1b2a8b34565bb5c2546a9b2b86b5dd1e
alain gonzales <peiju16@gmail.com>
2024-03-26 17:35:32 +0100

    gestion facture PDF and email sending

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Entity\OrderDetail;
+use App\Entity\OrderDetails;
 use App\Repository\OrderDetailRepository;
+use App\Repository\OrderDetailsRepository;
 use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,121 +22,150 @@ use Symfony\Component\Security\Core\Security;
 
 class PaymentController extends AbstractController
 {
-    
     #[Route('/payment', name: 'app_payment')]
-    public function index(Request $request, ProductRepository $productRepository, EntityManagerInterface $em, OrderRepository $orderRepository, Security $security): Response
-    {
+    public function index(
+        Request $request,
+        ProductRepository $productRepository,
+        EntityManagerInterface $entityManager,
+        OrderRepository $orderRepository,
+        Security $security
+    ): Response {
 
         if ($security->isGranted('IS_AUTHENTICATED_FULLY')) {
-        // je récupère la session cart
-        $cart = $request->getSession()->get('cart');
-        // je crée une commande
-        $order = new Order();
-        $carTotal = 0;
 
-        for ($i=0; $i < count($cart["id"]); $i++) { 
-           $carTotal += (float) $cart['price'][$i] * $cart["quantity"][$i];
-        }
+            // je récupère la session cart
+            $cart = $request->getSession()->get('cart');
+            // je créé une commande
+            $order = new Order;
+            $cartTotal = 0;
 
-        $order->setAmount($carTotal);
-        $order->setState('En cours');
-        $order->setUser($this->getUser());
-        $order->setDate(new\DateTime); 
-        
-        $em->persist($order);
-        $em->flush();
+            for ($i = 0; $i < count($cart["id"]); $i++) {
+                $cartTotal += (float) $cart["price"][$i] * $cart["quantity"][$i];
+            }
 
-        
-        // pour chaque élément de mon panier je créé un détail de commande
-        for ($i=0; $i < count($cart["id"]); $i++) { 
-           $orderDetail = new OrderDetail;
-           $orderDetail->setOrderNumber($orderRepository->findOneBy([],['id' => 'DESC']));
-           $orderDetail->setProduct($productRepository->find($cart['id'][$i]));
-           $orderDetail->setQuantity($cart["id"][$i]);
+            $order->setAmount($cartTotal);
+            $order->setState('En cours');
+            $order->setUser($this->getUser());
+            $order->setDate(new \DateTime);
+            $order->setPdf(false);
+            $entityManager->persist($order);
+            $entityManager->flush();
 
-           $em->persist($orderDetail);
-           $em->flush();
+            // pour chaque élément de mon panier je créé un détail de commande
+            for ($i = 0; $i < count($cart["id"]); $i++) {
+                $orderDetail = new OrderDetail;
+                $orderDetail->setOrderNumber($orderRepository->findOneBy([], ['id' => 'DESC']));
+                $orderDetail->setProduct($productRepository->find($cart["id"][$i]));
+                $orderDetail->setQuantity($cart["id"][$i]);
 
-           
+                $entityManager->persist($orderDetail);
+                $entityManager->flush();
+            }
 
-                // on génera le PDF
-                // on l'enverra par mail la facture
-                // on affichera une page de succès
-
-          return $this->redirectToRoute("sucess");
-
+            return $this->redirectToRoute("success");
 
         }
-
-    }
 
         $session = $request->getSession();
         $session->set('url_retour', $request->getUri());
 
         // si pas connecté
         return $this->redirectToRoute('app_login');
+
     }
 
     #[Route('/success', name: 'success')]
-    public function success(MailerInterface $mailer): Response
+    public function success(MailerInterface $mailer,
+    OrderRepository $orderRepository,
+    OrderDetailRepository $orderDetailsRepository,
+    EntityManagerInterface $entityManagerInterface,
+    Request $request): Response
     {
-        // le numéro de la dernière facture pour le user
+
+        $idUser = $this->getUser()->getId();
+        // le numéro de la dernière order pour le user
+        $order = $orderRepository->findOneBy(['user' => $idUser], ['id' => 'DESC']);
         // le montant total
         // les produits achetés
         // => récupérer la dernière facture insérée en bdd pour le user
-        // et tous les orderDetails liés à cette facture
+        // et tous les orderDetail liés à cette facture
 
+        if(!$order->isPdf()) {
 
-        // on génera le PDF
-        $pdfOptions = new Options();
-        $pdfOptions->set(['defaultFont' => 'Arial', 'enable_remote' => true]);
-        // 2- On crée le pdf avec les options
-        $dompdf = new Dompdf($pdfOptions);
+            // on génera le PDF
+            $pdfOptions = new Options();
+            $pdfOptions->set(['defaultFont' => 'Arial', 'enable_remote' => true]);
+            // 2- On crée le pdf avec les options
+            $dompdf = new Dompdf($pdfOptions);
 
-        // 3- On prépare le twig qui sera transformée en pdf
-        $html = $this->renderView('invoice/index.html.twig', [
-            'Amount' => 10,
-            'invoiceNumber' => 'F1093',
-            'date' => new \DateTime(),
-            'products' => []
-        ]);
+            $invoiceNumber = $order->getId();
+    
+            // 3- On prépare le twig qui sera transformée en pdf
+            $html = $this->renderView('invoice/index.html.twig', [
+                'user' => $this->getUser(),
+                'amount' => $order->getAmount(),
+                'invoiceNumber' => $invoiceNumber,
+                'date' => new \DateTime(),
+                'orderDetail' => $orderDetailsRepository->findBy(['orderNumber' => $order->getId()])
+            ]);
+    
+            // 4- On transforme le twig en pdf avec les options de format
+            $dompdf->loadHtml($html);
+            $dompdf->setPaper('A4', 'portrait');
+    
+            // 5- On enregistre le pdf dans une variable
+            $dompdf->render();
+            $finalInvoice = $dompdf->output();
+    
+            if (!file_exists('uploads/factures')) {
+                mkdir('uploads/factures');
+            }
 
-        // 4- On transforme le twig en pdf avec les options de format
-        $dompdf->loadHtml($html);
-        $dompdf->setPaper('A4', 'portrait');
-
-        // 5- On enregistre le pdf dans une variable
-        $dompdf->render();
-        $finalInvoice = $dompdf->output();
-
-        if (!file_exists('uploads/facture')) {
-            mkdir('uploads/factures');
-        }
-
-        $invoiceNumber = 5;
-        $pathInvoice = "./uploads/factures/" . $invoiceNumber . "_" . $this->getUser()->getId() . ".pdf";
-        file_put_contents($pathInvoice, $finalInvoice);
-        // on l'enverra par mail la facture
-        // on affichera une page de succès
-
-        $email = (new TemplatedEmail())
-            ->from($this->getParameter('app.mailAddress'))
-            ->to($this->getUser()->getEmail())
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject("Facture Blog Afpa 2024")
-            // ->html('<p> ' . $contact->getMessage() . ' </p>');
-            ->htmlTemplate("invoice/email.html.twig")
-            ->attach($finalInvoice, sprintf('facture-' . $invoiceNumber . 'blog-afpa.pdf', date("Y-m-d")));
-
+            $pathInvoice = "./uploads/factures/" . $invoiceNumber . "_" . $this->getUser()->getId() . ".pdf";
+            file_put_contents($pathInvoice, $finalInvoice);
+            // on l'enverra par mail la facture
+            // on affichera une page de succès
+    
+            $email = (new TemplatedEmail())
+                ->from($this->getParameter('app.mailAddress'))
+                ->to($this->getUser()->getEmail())
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject("Facture Blog Afpa 2024")
+                // ->html('<p> ' . $contact->getMessage() . ' </p>');
+                ->htmlTemplate("invoice/email.html.twig")
+                ->context([
+                    'user' => $this->getUser(),
+                    'amount' => $order->getAmount(),
+                    'invoiceNumber' => $invoiceNumber,
+                    'date' => new \DateTime(),
+                    'orderDetail' => $orderDetailsRepository->findBy(['orderNumber' => $order->getId()])
+                ])
+                ->attach($finalInvoice, sprintf('facture-' . $invoiceNumber . 'blog-afpa.pdf', date("Y-m-d")));
+    
             $mailer->send($email);
     
+            $order->setPdf(true);
+            $entityManagerInterface->persist($order);
+            $entityManagerInterface->flush();
+    
+            // vider le panier
+            $session = $request->getSession();
+            $session->set('cart', []);        
+    
             return $this->render("payment/success.html.twig", [
+                'user' => $this->getUser(),
+                'amount' => $order->getAmount(),
                 'invoiceNumber' => $invoiceNumber,
-                'amount' => 100,
+                'date' => new \DateTime(),
+                'orderDetail' => $orderDetailsRepository->findBy(['orderNumber' => $order->getId()])
             ]);
 
-       
+        } else {
+
+            return $this->redirectToRoute('app_home');
+        }
+
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -20,7 +20,7 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
+    #[Route('/product/new', name: 'app_product_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
@@ -49,7 +49,7 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
+    #[Route('/product/edit/{id}', name: 'app_product_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ProductType::class, $product);
@@ -68,7 +68,7 @@ class ProductController extends AbstractController
     }
 
     
-    #[Route('/{id}', name: 'app_product_delete', methods: ['POST'])]
+    #[Route('/product/delete/{id}', name: 'app_product_delete', methods: ['POST'])]
     public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
